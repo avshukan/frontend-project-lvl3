858c6e81b153b0adf1004a623920badfd04cfe8f
avshukan <avshukan@gmail.com>
2022-04-17 15:25:08 +1100

    try fix test-coverage

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,9 @@
-const { test, expect, describe } = require('@jest/globals');
+import { test, expect, describe } from '@jest/globals';
+import tmp from '../index.js';
 
 describe('mock', () => {
   test('test', () => {
     expect(true).toBeTruthy();
+    expect(tmp(3, 4)).toBe(12);
   });
 });
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('Hello, world!');
+
+const tmp = (a, b) => a * b;
+
+console.log(tmp(11, 13));
+
+export default tmp;
