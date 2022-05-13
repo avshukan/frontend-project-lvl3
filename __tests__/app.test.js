5677426b56ff238e17a5c93a1286f683135847bd
avshukan <avshukan@gmail.com>
2022-05-13 16:17:38 +1100

    try jest-puppeteer

--- __tests__/app.test.js
+++
@@ -1,9 +0,0 @@
-import { test, expect, describe } from '@jest/globals';
-import app from '../src/app.js';
-
-describe('mock', () => {
-  test('test', () => {
-    expect(true).toBeTruthy();
-    expect(() => { app(); }).toThrow();
-  });
-});
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const config = {
+  preset: 'jest-puppeteer',
+  globals: {
+    URL: 'http://localhost:8080',
+  },
+  verbose: true,
+};
+
+export default config;
