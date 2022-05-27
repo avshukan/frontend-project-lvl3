f774db40bef4d347b30e00275ecd584ba9d3c393
avshukan <avshukan@gmail.com>
2022-05-27 22:04:50 +1100

    perform makeUrlWithProxy

--- src/makeUrlWithProxy.js
+++ src/makeUrlWithProxy.js
@@ -1,1 +1,6 @@
-export default (url) => `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`;
+export default (url) => {
+  const proxyUrl = new URL('/get', 'https://allorigins.hexlet.app');
+  proxyUrl.searchParams.set('disableCache', 'true');
+  proxyUrl.searchParams.set('url', url);
+  return proxyUrl.toString();
+};
